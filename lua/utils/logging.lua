da15ca3e540148c1c5e92dc4fbecb6e1928ffa50
Rob Taylor <manix84@gmail.com>
2020-07-05 01:35:37 +0100

    Some debug logging cleanup.

--- lua/utils/logging.lua
+++ lua/utils/logging.lua
@@ -1,9 +1,9 @@
 print('[Discord] Loading: utils/logging.lua')
 
-local enableDebugLogging = true;
+local enableDebugLogging = true
 
-function print_debug(msg)
+function print_debug(message)
   if (enableDebugLogging) then
-    print('[Discord]' .. msg)
+    print('[Discord]' .. message)
   end
 end
