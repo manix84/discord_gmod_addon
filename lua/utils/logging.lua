851aa6b8c609a2cb97ecc76decb91532699762ca
Rob Taylor <manix84@gmail.com>
2020-07-07 00:14:01 +0100

    Expanding print_debug logic to allow for multiple arguments.

--- lua/utils/logging.lua
+++ lua/utils/logging.lua
@@ -2,8 +2,8 @@ CreateConVar("discord_debug", 0, 1, "Print debug messages to console.")
 
 local enableDebugLogging = GetConVar("discord_debug"):GetBool()
 
-function print_debug(message)
+function print_debug(...)
   if (enableDebugLogging) then
-    print('[Discord]' .. message)
+    print('[Discord]', ...)
   end
 end
