cbb9eb0cdf29873d0079bf0b41c63093a655ab81
Rob Taylor <manix84@gmail.com>
2020-07-07 10:07:26 +0100

    Unifying messages quotes.

--- lua/locale/english.lua
+++ lua/locale/english.lua
@@ -6,10 +6,10 @@ local english = {
     JOIN_DISCORD_PROMPT = "Join the discord server - %s",
     CONNECTION_INSTRUCTIONS = "Then link up by saying '!discord DISCORD_NAME' in the chat. E.g. '!discord Manix84'",
     NAME_NOT_FOUND = "There is no one on the Discord server with a name like '%s'.",
-    MULTIPLE_NAMES_FOUND = "Found more than one user with a name like '%s'. Try your full tag, EG: Manix84#1234",
+    MULTIPLE_NAMES_FOUND = "Found more than one user with a name like '%s'. Try your full tag, EG: '!discord Manix84#1234'",
     CONNECTION_SUCCESSFUL = "'%s' (%s) successfully bound to '%s'",
     WELCOME_CONNECTED = "You are connected with discord.",
-    AUTOMATIC_MATCH = "Attempting to match your name, %s",
+    AUTOMATIC_MATCH = "Attempting to match your name, '%s'",
 }
 
 return english
