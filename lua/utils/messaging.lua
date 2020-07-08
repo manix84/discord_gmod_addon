2c2dc2e7a14df98304307929e8959a9eae704908
Rob Taylor <manix84@gmail.com>
2020-07-08 16:12:09 +0100

    Getting translations on message. This allows for translation changes.

--- lua/utils/messaging.lua
+++ lua/utils/messaging.lua
@@ -1,13 +1,14 @@
 include('./locale.lua')
-local translations = getTranslations()
 
 function playerMessage(translation_key, target_ply, ...)
+  local translations = getTranslations()
   local translation_string = translations[translation_key] or ''
   local message = string.format(translation_string, ...)
   target_ply:PrintMessage(HUD_PRINTTALK, "["..GetConVar("discord_name"):GetString().."] " .. message)
 end
 
 function announceMessage(translation_key, ...)
+  local translations = getTranslations()
   local translation_string = translations[translation_key] or ''
   local message = string.format(translation_string, ...)
   PrintMessage(HUD_PRINTTALK, "["..GetConVar("discord_name"):GetString().."] " .. message)
