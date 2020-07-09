526005e96339ad40bab5f3a937d9aeb6aec44a74
Rob Taylor <manix84@gmail.com>
2020-07-09 08:57:52 +0100

    Adding Translation Missing message.

--- lua/utils/messaging.lua
+++ lua/utils/messaging.lua
@@ -2,14 +2,14 @@ include('./locale.lua')
 
 function playerMessage(translation_key, target_ply, ...)
   local translations = getTranslations()
-  local translation_string = translations[translation_key] or ''
+  local translation_string = translations[translation_key] or '!!TRANSLATION MISSING!!'
   local message = string.format(translation_string, ...)
   target_ply:PrintMessage(HUD_PRINTTALK, "["..GetConVar("discord_name"):GetString().."] " .. message)
 end
 
 function announceMessage(translation_key, ...)
   local translations = getTranslations()
-  local translation_string = translations[translation_key] or ''
+  local translation_string = translations[translation_key] or '!!TRANSLATION MISSING!!'
   local message = string.format(translation_string, ...)
   PrintMessage(HUD_PRINTTALK, "["..GetConVar("discord_name"):GetString().."] " .. message)
 end
