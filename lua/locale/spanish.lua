0c40a508052e9ab847b69abf4e6270b04105b03a
Rob Taylor <manix84@gmail.com>
2020-07-28 14:23:00 +0100

    Updating the variable name to spanish.

--- lua/locale/spanish.lua
+++ lua/locale/spanish.lua
@@ -1,4 +1,4 @@
-local english = {
+local spanish = {
     MUTED_FOR_DURATION = "Estás silenciado por %s segundos.",
     MUTED_FOR_ROUND = "Estás silenciado hasta que acabe la ronda.",
     UNMUTED = "Ya no estás silenciado.",
@@ -12,4 +12,4 @@ local english = {
     AUTOMATIC_MATCH = "Intentando coincidir con tu nombre, '%s'",
 }
 
-return english
+return spanish
